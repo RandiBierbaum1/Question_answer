3fbd94af573a7f8d1ba8d5e294d2be5d7ead928a
RandiBierbaum <randibierbaum@gmail.com>
2019-11-14 10:38:58 +0100

    mongoose.connect

--- server/api.js
+++ server/api.js
@@ -93,10 +93,9 @@ app.get('*', (request, response) =>
 
 
 /**** Start ****/
-let url = (process.env.MONGO_URL || 'mongodb://localhost/question_db');
-
-mongoose.connect('mongodb+srv://randi:Sommerfugl89@cluster0-retun.mongodb.net/test?retryWrites=true&w=majority',
-    {useNewUrlParser: true, useUnifiedTopology: true})
+let url = (process.env.MONGODB_URL || 'mongodb://localhost/question_db');
+//mongoose.connect('mongodb+srv://randi:Sommerfugl89@cluster0-retun.mongodb.net/test?retryWrites=true&w=majority',
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(async () => {
         await app.listen(port); // Start the API
         console.log(`Question API running on port ${port}!`)
